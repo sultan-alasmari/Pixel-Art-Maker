98be97d608c0774b1ab038e69d920f6d3ec3d051
sultan-alasmari <62773454+sultan-alasmari@users.noreply.github.com>
2020-06-16 04:43:17 +0300

    Add files via upload
    Pixel Art Maker project

---
+++ designs.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+
+const colorPicker = document.getElementById('colorPicker');
+const rowsNumber = document.getElementById('inputHeight');
+const cellsNumber = document.getElementById('inputWidth');
+const pixelCanvas = document.getElementById('pixelCanvas');
+const form = document.getElementById('sizePicker');
+
+// When size is submitted by the user, call makeGrid()
+form.addEventListener('submit', function(e) {
+    pixelCanvas.innerHTML = '';
+    e.preventDefault();
+    makeGrid();
+
+});
+
+pixelCanvas.addEventListener('click', function(e) {
+    if (e.target.nodeName === 'TD') {
+        e.target.style.backgroundColor = colorPicker.value;
+    }
+
+});
+
+function makeGrid() {
+    for (let i = 0; i < rowsNumber.value; i++) {
+        const row = pixelCanvas.insertRow(0);
+        for (let j = 0; j < cellsNumber.value; j++) {
+            row.insertCell(0);
+        }
+
+    }
+
+
+
+};
